16ded67e08a81d9613d20fde57ad3b72290004e0
Abinandhini_A <74504848+Abinandhini-cpu@users.noreply.github.com>
2025-03-24 10:56:41 +0530

    Update user.py

--- libs/ktem/ktem/pages/resources/user.py
+++ libs/ktem/ktem/pages/resources/user.py
@@ -324,8 +324,6 @@ class UserManagement(BasePage):
             if not user.admin:
                 return [], pd.DataFrame.from_records(
                     [{"id": "-", "username": "-", "admin": "-"}]
-                    user==123
-                    password=test
                 )
 
             statement = select(User)
